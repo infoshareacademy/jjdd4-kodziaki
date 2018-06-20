e072ae6aa74d828cee47e190567b0af8f20b2aad
pawelpinker <pawel.pinker@gmail.com>
2018-06-20 16:45:58 +0200

    Initial commit

---
+++ src/main/java/com/infoshare/kodziaki/App.java
@@ -0,0 +1,12 @@
+package com.infoshare.kodziaki;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
