759931fff179af428a80f76448002f2e9aa61279
pawelpinker <pawel.pinker@gmail.com>
2018-06-20 16:42:10 +0200

    Usunięcie Aplikacji - czyszczenie branchy

--- Aplikacja/src/com/infoshareacademy/kodziaki/io/Main.java
+++
@@ -1,9 +0,0 @@
-package com.infoshareacademy.kodziaki.io;
-
-public class Main {
-
-    public static void main(String[] args) {
-        
-
-    }
-}
--- Aplikacja/src/com/infoshareacademy/kodziaki/model/PlaceType.java
+++
@@ -1,7 +0,0 @@
-package com.infoshareacademy.kodziaki.model;
-
-public enum PlaceType {
-    APARTMENT,
-    ROOM,
-    BED
-}
