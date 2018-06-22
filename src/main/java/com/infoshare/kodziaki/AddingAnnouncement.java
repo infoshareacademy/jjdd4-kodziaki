a006b2db460751b6504830cd8a542fbbd7fc468b
Mikolaj Andrzejewski <mikolaj.andrzejewski91@wp.pl>
2018-06-22 16:53:10 +0200

    almost done

--- src/main/java/com/infoshare/kodziaki/AddingAnnouncement.java
+++ src/main/java/com/infoshare/kodziaki/AddingAnnouncement.java
@@ -1,26 +1,36 @@
 package com.infoshare.kodziaki;
 
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class AddingAnnouncement {
 
-    public static void adding(){
+    public static void adding() {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Podaj tytuł ogłoszenia");
-        String title= scanner.nextLine();
+        String title = scanner.nextLine();
 
-        System.out.println("Podaj rodzaj zakraterowania: APARTMENT/ROOM/BED");
-        String placeType= scanner.nextLine();
+        System.out.println("Podaj rodzaj zakraterowania: apartment/room/bed");
+        String placeType=null;
+        boolean isOk = false;
+        while(!isOk) {
+            placeType = scanner.nextLine();
+            if("apartment".equals(placeType) ||"room".equals(placeType) || "bed".equals(placeType)) {
+                isOk = true;
+            } else {
+                System.out.println("Blad, sprobuj jeszcze raz");
+            }
+        }
 
         System.out.println("Podaj cene");
-        BigDecimal price = new BigDecimal(scanner.nextInt());
+        BigDecimal price = scanner.nextBigDecimal();
 
-        System.out.println("Podaj powierzchnie");
-        double area = scanner.nextInt();
+        System.out.println("Podaj powierzchnie w m kw.");
+        double area = scanner.nextDouble();
 
         System.out.println("Podaj liczbe pokoi");
         int rooms = scanner.nextInt();
@@ -28,28 +38,80 @@ public class AddingAnnouncement {
         System.out.println("Podaj piętro mieszkania");
         int floor = scanner.nextInt();
 
-        System.out.println("Podaj dzielnice");
-        String district = scanner.nextLine();
+        Scanner scanner1 = new Scanner(System.in);
+
+        System.out.println("Podaj dzielnice miasta");
+        String district = scanner1.nextLine();
 
         System.out.println("Podaj miasto");
-        String miasto = scanner.nextLine();
+        String city = scanner1.nextLine();
 
-        System.out.println("Czy w budynku jest winda? (true/false");
-        String hasElevator = scanner.nextLine();
+        System.out.println("Czy w budynku jest winda? (true/false)");
+        String hasElevator=null;
+        boolean isOk1 = false;
+        while(!isOk1) {
+            hasElevator = scanner1.nextLine();
+            if("true".equals(hasElevator) ||"false".equals(hasElevator)) {
+                isOk1 = true;
+            } else {
+                System.out.println("Blad, sprobuj jeszcze raz");
+            }
+        }
 
         System.out.println("Czy palenie w mieszkaniu jest dozwolone? (true/false)");
-        String smokingAllowed = scanner.nextLine();
+        String smokingAllowed=null;
+        boolean isOk2 = false;
+        while(!isOk2) {
+            smokingAllowed = scanner1.nextLine();
+            if("true".equals(smokingAllowed) ||"false".equals(smokingAllowed)) {
+                isOk2 = true;
+            } else {
+                System.out.println("Blad, sprobuj jeszcze raz");
+            }
+        }
 
-        System.out.println("Czy dozwolone sa zwierzeta w budynku? (true/false");
-        String animalsAllowed = scanner.nextLine();
+        System.out.println("Czy dozwolone sa zwierzeta w budynku? (true/false)");
+        String animalsAllowed=null;
+        boolean isOk3 = false;
+        while(!isOk3) {
+            animalsAllowed = scanner1.nextLine();
+            if("true".equals(animalsAllowed) ||"false".equals(animalsAllowed)) {
+                isOk3 = true;
+            } else {
+                System.out.println("Blad, sprobuj jeszcze raz");
+            }
+        }
 
         System.out.println("Czy wynajem dlugoterminowy? (true/false)");
-        String onlyLongTerm = scanner.nextLine();
+        String onlyLongTerm=null;
+        boolean isOk4 = false;
+        while(!isOk4) {
+            onlyLongTerm = scanner1.nextLine();
+            if("true".equals(onlyLongTerm) ||"false".equals(onlyLongTerm)) {
+                isOk4 = true;
+            } else {
+                System.out.println("Blad, sprobuj jeszcze raz");
+            }
+        }
 
         System.out.println("Dodaj opis mieszkania");
-        String description = scanner.nextLine();
+        String description = scanner1.nextLine();
+
+        try {
+
+            double id = Math.random();
+
+            FileWriter file = new FileWriter("/home/mikolajandrzejewski/Projekt Kodziaki/jjdd4-kodziaki/files/ads.csv", true);
+            BufferedWriter add = new BufferedWriter(file);
+            add.write(id + ";" + title + ";" + placeType + ";" + price + ";" + area + ";" + rooms + ";" + floor + ";" + district + ";" + city + ";" + hasElevator + ";" + smokingAllowed + ";" + animalsAllowed + ";" + onlyLongTerm + ";" + description);
+            add.newLine();
+            add.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
     }
-
 }
-
 
--- src/main/java/com/infoshare/kodziaki/App.java
+++ src/main/java/com/infoshare/kodziaki/App.java
@@ -1,14 +1,12 @@
 package com.infoshare.kodziaki;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args ) {
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException{
 
         AddingAnnouncement ad1 = new AddingAnnouncement();
+
         ad1.adding();
     }
 }
