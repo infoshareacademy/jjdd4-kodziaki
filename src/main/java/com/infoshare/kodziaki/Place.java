7c4e569c109cf03666f3efbd81b967e8e6951e54
Marta Kostyk <martakostyk@op.pl>
2018-06-22 15:32:52 +0200

    class Place updated, getters and constructor added

--- src/main/java/com/infoshare/kodziaki/Place.java
+++ src/main/java/com/infoshare/kodziaki/Place.java
@@ -3,7 +3,8 @@ package com.infoshare.kodziaki;
 import java.math.BigDecimal;
 
 public class Place {
-    int id;
+
+    private int id;
     private placeType placeType;
 
     private BigDecimal price;
@@ -21,4 +22,64 @@ public class Place {
     private boolean smokingAllowed;
     private boolean animalAllowed;
     private boolean onlyLongTerm;
+
+    public Place() {}
+
+    public Place(int id,
+                 String title,
+                 placeType placeType,
+                 BigDecimal price,
+                 double area,
+                 int rooms,
+                 int floor,
+                 String district,
+                 String city,
+                 boolean hasElevator,
+                 boolean smokingAllowed,
+                 boolean animalAllowed,
+                 boolean onlyLongTerm,
+                 String description) {
+        this.id = id;
+        this.title = title;
+        this.placeType = placeType;
+        this.price = price;
+        this.area = area;
+        this.rooms = rooms;
+        this.floor = floor;
+        this.district = district;
+        this.city = city;
+        this.hasElevator = hasElevator;
+        this.smokingAllowed = smokingAllowed;
+        this.animalAllowed = animalAllowed;
+        this.onlyLongTerm = onlyLongTerm;
+        this.description = description;
+    }
+
+    public int getId() { return id; }
+
+    public com.infoshare.kodziaki.placeType getPlaceType() { return placeType; }
+
+    public BigDecimal getPrice() { return price; }
+
+    public double getArea() { return area; }
+
+    public int getRooms() { return rooms; }
+
+    public int getFloor() { return floor; }
+
+    public String getDistrict() { return district; }
+
+    public String getCity() { return city; }
+
+    public String getDescription() { return description; }
+
+    public String getTitle() { return title; }
+
+    public boolean isHasElevator() { return hasElevator; }
+
+    public boolean isSmokingAllowed() { return smokingAllowed; }
+
+    public boolean isAnimalAllowed() { return animalAllowed; }
+
+    public boolean isOnlyLongTerm() { return onlyLongTerm; }
 }
