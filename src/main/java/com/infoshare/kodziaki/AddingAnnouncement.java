1d940c206727cd2c772add758bc92450a5b08291
Mikolaj Andrzejewski <mikolaj.andrzejewski91@wp.pl>
2018-06-25 16:47:59 +0200

    Adding Announcement ready

--- src/main/java/com/infoshare/kodziaki/AddingAnnouncement.java
+++ src/main/java/com/infoshare/kodziaki/AddingAnnouncement.java
@@ -3,110 +3,164 @@ package com.infoshare.kodziaki;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
+import static java.util.stream.Collectors.joining;
+
 public class AddingAnnouncement {
+
+    public static String readFromUser(String question, List<String> options) {
+        Scanner scanner = new Scanner(System.in);
+        String input = null;
+
+        System.out.print(question + " (" + options.stream().collect(joining(", ")) + "): ");
+
+        while (true) {
+            input = scanner.nextLine();
+            if (options.contains(input)) {
+                break;
+            } else {
+                System.out.print("Poprawne wartości to: " + options.stream().collect(joining(", ")) + " - Spróbuj jeszcze raz: " );
+            }
+        }
+        return input;
+    }
 
     public static void adding() {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Podaj tytuł ogłoszenia");
-        String title = scanner.nextLine();
-
-        System.out.println("Podaj rodzaj zakraterowania: apartment/room/bed");
-        String placeType=null;
-        boolean isOk = false;
-        while(!isOk) {
-            placeType = scanner.nextLine();
-            if("apartment".equals(placeType) ||"room".equals(placeType) || "bed".equals(placeType)) {
-                isOk = true;
-            } else {
-                System.out.println("Blad, sprobuj jeszcze raz");
+        System.out.print("Tytuł ogłoszenia: ");
+        String title = null;
+        while (true){
+            try {
+                title = new String(scanner.nextLine());
+                break;
+            } catch (Exception e){
+                System.out.print("Coś poszło nie tak, spróbuj jeszcze raz: ");
             }
         }
 
-        System.out.println("Podaj cene");
-        BigDecimal price = scanner.nextBigDecimal();
-
-        System.out.println("Podaj powierzchnie w m kw.");
-        double area = scanner.nextDouble();
-
-        System.out.println("Podaj liczbe pokoi");
-        int rooms = scanner.nextInt();
-
-        System.out.println("Podaj piętro mieszkania");
-        int floor = scanner.nextInt();
-
-        Scanner scanner1 = new Scanner(System.in);
-
-        System.out.println("Podaj dzielnice miasta");
-        String district = scanner1.nextLine();
-
-        System.out.println("Podaj miasto");
-        String city = scanner1.nextLine();
-
-        System.out.println("Czy w budynku jest winda? (true/false)");
-        String hasElevator=null;
-        boolean isOk1 = false;
-        while(!isOk1) {
-            hasElevator = scanner1.nextLine();
-            if("true".equals(hasElevator) ||"false".equals(hasElevator)) {
-                isOk1 = true;
-            } else {
-                System.out.println("Blad, sprobuj jeszcze raz");
+        System.out.print("Podaj miasto: ");
+        String city = null;
+        while (true){
+            try {
+                city = new String(scanner.nextLine());
+                break;
+            } catch (Exception e){
+                System.out.print("Coś poszło nie tak, spróbuj jeszcze raz: ");
             }
         }
 
-        System.out.println("Czy palenie w mieszkaniu jest dozwolone? (true/false)");
-        String smokingAllowed=null;
-        boolean isOk2 = false;
-        while(!isOk2) {
-            smokingAllowed = scanner1.nextLine();
-            if("true".equals(smokingAllowed) ||"false".equals(smokingAllowed)) {
-                isOk2 = true;
-            } else {
-                System.out.println("Blad, sprobuj jeszcze raz");
+        System.out.print("Podaj dzielnice miasta: ");
+        String district = null;
+        while (true){
+            try{
+                district = new String(scanner.nextLine());
+                break;
+            } catch (Exception e){
+                System.out.print("Coś poszło nie tak, spróbuj jeszcze raz: ");
             }
         }
 
-        System.out.println("Czy dozwolone sa zwierzeta w budynku? (true/false)");
-        String animalsAllowed=null;
-        boolean isOk3 = false;
-        while(!isOk3) {
-            animalsAllowed = scanner1.nextLine();
-            if("true".equals(animalsAllowed) ||"false".equals(animalsAllowed)) {
-                isOk3 = true;
-            } else {
-                System.out.println("Blad, sprobuj jeszcze raz");
+        String placeType = readFromUser("Rodzaj zakwaterowania: ", Arrays.asList("apartment", "room", "bed"));
+
+        System.out.print("Podaj powierzchnię w m kw.: ");
+        Double area = null;
+        while (true){
+            try {
+                area = new Double(scanner.nextLine());
+                break;
+            } catch (Exception e) {
+                System.out.print("Coś poszło nie tak, spróbuj jeszcze raz: ");
             }
         }
 
-        System.out.println("Czy wynajem dlugoterminowy? (true/false)");
-        String onlyLongTerm=null;
-        boolean isOk4 = false;
-        while(!isOk4) {
-            onlyLongTerm = scanner1.nextLine();
-            if("true".equals(onlyLongTerm) ||"false".equals(onlyLongTerm)) {
-                isOk4 = true;
-            } else {
-                System.out.println("Blad, sprobuj jeszcze raz");
+        System.out.print("Liczba pokoi: ");
+        Integer rooms = null;
+        while (true){
+            try{
+                rooms = new Integer(scanner.nextLine());
+                break;
+            }catch (Exception e) {
+                System.out.print("Coś poszło nie tak, spróbuj jeszcze raz: ");
             }
         }
 
-        System.out.println("Dodaj opis mieszkania");
-        String description = scanner1.nextLine();
+        System.out.print("Podaj piętro mieszkania: ");
+        Integer floor = null;
+        while (true){
+            try {
+                floor = new Integer(scanner.nextLine());
+                break;
+            }catch (Exception e){
+                System.out.print("Coś poszło nie tak, spróbuj jeszcze raz: ");
+            }
+        }
+
+        String hasElevator = readFromUser("Czy w budynku jest winda?", Arrays.asList("true", "false"));
+
+        String smokingAllowed = readFromUser("Czy palenie w budynku jest doswolone?", Arrays.asList("true", "false"));
+
+        String animalsAllowed = readFromUser("Czy zwierzęta w budynku są dozwolone?", Arrays.asList("true", "false"));
+
+        String onlyLongTerm = readFromUser("Czy preferujesz wynajem długoterminowy", Arrays.asList("true", "false"));
+
+        System.out.print("Cena: ");
+        BigDecimal price = null;
+        while (true) {
+            try {
+                price = new BigDecimal(scanner.nextLine());
+                break;
+            } catch (Exception e) {
+                System.out.print("Coś poszło nie tak, spróbuj jeszcze raz: ");
+            }
+        }
+
+        System.out.print("Dodatkowy opis mieszkania: ");
+        String description = null;
+        while (true){
+            try{
+                description = new String(scanner.nextLine());
+                break;
+            } catch (Exception e){
+                System.out.print("Coś poszło nie tak, spróbuj jeszcze raz: ");
+            }
+        }
+
+        System.out.print("Podaj swoje imię i nazwisko: ");
+        String author = null;
+        while (true){
+            try {
+                author = new String(scanner.nextLine());
+                break;
+            } catch (Exception e){
+                System.out.print("Coś poszło nie tak, spróbuj jeszcze raz: ");
+            }
+        }
+
+        System.out.print("Podaj numer telefonu: ");
+        Integer phoneNumber = null;
+        while (true){
+            try {
+                phoneNumber = new Integer(scanner.nextLine());
+                break;
+            } catch (Exception e){
+                System.out.print("Coś poszło nie tak, spróbuj jeszcze raz: ");
+            }
+        }
 
         try {
-
-            double id = Math.random();
+            Random id = new Random();
 
             FileWriter file = new FileWriter("/home/mikolajandrzejewski/Projekt Kodziaki/jjdd4-kodziaki/files/ads.csv", true);
             BufferedWriter add = new BufferedWriter(file);
-            add.write(id + ";" + title + ";" + placeType + ";" + price + ";" + area + ";" + rooms + ";" + floor + ";" + district + ";" + city + ";" + hasElevator + ";" + smokingAllowed + ";" + animalsAllowed + ";" + onlyLongTerm + ";" + description);
+            add.write(id.nextInt() + ";" + title + ";" + placeType + ";" + price + ";" + area + ";" + rooms + ";" + floor + ";" + district + ";" + city + ";" + hasElevator + ";" + smokingAllowed + ";" + animalsAllowed + ";" + onlyLongTerm + ";" + description+ ";" +author+ ";" + phoneNumber);
             add.newLine();
             add.close();
-
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
