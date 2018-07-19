c8d48504617f9c6a52f8147037d73ed127fd33a6
Mikolaj <mikolaj.andrzejewski91@wp.pl>
2018-07-19 21:04:47 +0200

    few amendments to merge to master - still working on

--- webapp/src/main/java/AddAnnouncementServlet.java
+++ webapp/src/main/java/AddAnnouncementServlet.java
@@ -1,72 +1,65 @@
 import com.infoshare.kodziaki.dao.PlaceDao;
 import com.infoshare.kodziaki.model.Place;
 import com.infoshare.kodziaki.model.PlaceType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.math.BigDecimal;
 
 @WebServlet(urlPatterns = "/add-announcement")
 public class AddAnnouncementServlet extends HttpServlet {
 
+    private Logger LOG = LoggerFactory.getLogger(AddAnnouncementServlet.class);
+
     @Inject
     private PlaceDao placeDao;
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         String titleParam = req.getParameter("title");
-
         PlaceType placeTypeParam = PlaceType.valueOf(req.getParameter("placeType").toUpperCase());
-
         BigDecimal priceParam = new BigDecimal(req.getParameter("price"));
-
         Double areaParam = Double.parseDouble(req.getParameter("area"));
-
         Integer roomsParam = Integer.valueOf(req.getParameter("rooms"));
-
         Integer floorParam = Integer.valueOf(req.getParameter("floor"));
-
         String districtParam = req.getParameter("district");
-
         String cityParam = req.getParameter("city");
-
         Boolean hasElevatorParam = Boolean.valueOf(req.getParameter("hasElevator"));
-
         Boolean smokingAllowedParam = Boolean.valueOf(req.getParameter("smokingAllowed"));
-
         Boolean animalAllowedParam = Boolean.valueOf(req.getParameter("animalAllowed"));
-
         Boolean onlyLongTermParam = Boolean.valueOf(req.getParameter("onlyLongTerm"));
-
         String descriptionParam = req.getParameter("description");
-
         String authorParam = req.getParameter("author");
-
         String phoneNumberParam = req.getParameter("phoneNumber");
 
-        final Place place1 =  new Place();
+        final Place place =  new Place();
 
-        place1.setTitle(titleParam);
-        place1.setPlaceType(placeTypeParam);
-        place1.setPrice(priceParam);
-        place1.setArea(areaParam);
-        place1.setRooms(roomsParam);
-        place1.setFloor(floorParam);
-        place1.setDistrict(districtParam);
-        place1.setCity(cityParam);
-        place1.setHasElevator(hasElevatorParam);
-        place1.setSmokingAllowed(smokingAllowedParam);
-        place1.setAnimalAllowed(animalAllowedParam);
-        place1.setOnlyLongTerm(onlyLongTermParam);
-        place1.setDescription(descriptionParam);
-        place1.setAuthor(authorParam);
-        place1.setPhoneNumber(phoneNumberParam);
+        place.setTitle(titleParam);
+        place.setPlaceType(placeTypeParam);
+        place.setPrice(priceParam);
+        place.setArea(areaParam);
+        place.setRooms(roomsParam);
+        place.setFloor(floorParam);
+        place.setDistrict(districtParam);
+        place.setCity(cityParam);
+        place.setHasElevator(hasElevatorParam);
+        place.setSmokingAllowed(smokingAllowedParam);
+        place.setAnimalAllowed(animalAllowedParam);
+        place.setOnlyLongTerm(onlyLongTermParam);
+        place.setDescription(descriptionParam);
+        place.setAuthor(authorParam);
+        place.setPhoneNumber(phoneNumberParam);
 
-        placeDao.save(place1);
+        placeDao.save(place);
+        LOG.info("Saved new announcement ", place);
 
         }
 }
