86ff08c40a9ba34106948811930498f0ce3d644d
Dawid Murawski <dawid.murawski1@gmail.com>
2018-07-30 16:39:18 +0200

    Starting with generating report

---
+++ webapp/src/main/java/com/infoshare/kodziaki/web/statistics/StatisticReport.java
@@ -0,0 +1,10 @@
+package com.infoshare.kodziaki.web.statistics;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "")
+public class StatisticReport {
+
+}
