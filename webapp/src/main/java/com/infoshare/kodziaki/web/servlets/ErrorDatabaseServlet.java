10913ee8e9d2041467ae24999e23c34ce60f9c93
P-Pinker <pawel.pinker@gmail.com>
2018-08-03 00:25:22 +0200

    Added error freemarker templates.

---
+++ webapp/src/main/java/com/infoshare/kodziaki/web/servlets/ErrorDatabaseServlet.java
@@ -0,0 +1,47 @@
+package com.infoshare.kodziaki.web.servlets;
+
+import com.infoshare.kodziaki.web.dao.PlaceDao;
+import com.infoshare.kodziaki.web.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/error-db")
+public class ErrorDatabaseServlet extends HttpServlet {
+
+    private Logger LOG = LoggerFactory.getLogger(MainPageServlet.class);
+
+    @Inject
+    private PlaceDao placeDao;
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Template template = templateProvider.getTemplate(getServletContext(), "ErrorDatabase.ftlh");
+        Map<String, Object> dataModel = new HashMap<>();
+
+        dataModel.put("isLoggedIn", request.getSession().getAttribute("userLogged"));
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        try {
+            template.process(dataModel, response.getWriter());
+        } catch (TemplateException e) {
+            LOG.error("Template not found", e.getMessage());
+            response.sendRedirect("/error");
+        }
+    }
+}
